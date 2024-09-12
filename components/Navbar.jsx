8c6065bbe66cfd16ce329c4395d96017781c8705
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-09-12 17:46:19 +0300

    style: navbar updated

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 
-const linkStyle = "text-xl font-bold uppercase text-neutral-400 relative overflow-hidden";
+const linkStyle = "text-3xl font-bold uppercase text-neutral-400 relative overflow-hidden";
 
 const navbarVariants = {
     hidden: { opacity: 0, x: '-100%' },
@@ -9,24 +9,32 @@ const navbarVariants = {
 };
 
 const linkVariants = {
-    hover: { scale: 1.1, transition: { duration: 0.3 } },
-    initial: { scale: 1, transition: { duration: 0.3 } },
+    hover: { 
+        scale: 1.1, 
+        color: '#e5e7eb', // neutral-200
+        transition: { duration: 0.3 } 
+    },
+    initial: { 
+        scale: 1, 
+        color: '#9ca3af', // neutral-400
+        transition: { duration: 0.3 } 
+    },
 };
 
 function Navbar() {
     return (
         <motion.div 
-            className="fixed w-full bg-white/10 backdrop-blur-md shadow-md py-6 px-12 flex justify-between items-center z-20"
+            className="fixed w-full backdrop-blur-md shadow-md py-6 px-12 flex justify-between items-center z-20"
             variants={navbarVariants}
             initial="hidden"
             animate="visible"
             transition={{ duration: 0.5, ease: "easeOut" }}
         >
-            <div className="text-5xl font-extrabold tracking-wide text-transparent bg-clip-text bg-gradient-to-r from-neutral-50 to-neutral-300">
+            <div className="text-5xl font-extrabold tracking-wide text-transparent bg-clip-text bg-gradient-to-r from-neutral-400 to-neutral-200">
                 <span>EventNest.</span>
             </div>
-            <div className="space-x-4">
-                {["Home", "About", "Services", "Contact"].map((item) => (
+            <div className="flex items-center gap-12">
+                {["Home.", "About.", "Services.", "Contact."].map((item) => (
                     <motion.a 
                         href="#" 
                         key={item}
@@ -36,13 +44,6 @@ function Navbar() {
                         whileHover="hover"
                     >
                         {item}
-                        <motion.div
-                            className="absolute inset-0 bg-violet-700 bg-opacity-50 opacity-0 blur-sm rounded-full transition-all duration-300"
-                            variants={{
-                                hover: { opacity: 0.5, scale: 1.2 },
-                                initial: { opacity: 0, scale: 1 }
-                            }}
-                        />
                     </motion.a>
                 ))}
             </div>
