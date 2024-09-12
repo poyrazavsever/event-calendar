8912cc7c9c42da151cf6979d7fa89199709bf96a
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-09-12 18:08:15 +0300

    fix: cursor problem

--- components/Scroll.jsx
+++ components/Scroll.jsx
@@ -3,7 +3,7 @@ import { FaMouse } from 'react-icons/fa'; // Mouse ikonunu import ettik
 
 const Scroll = () => {
     return (
-        <div className="fixed bottom-5 right-5 flex items-center justify-center gap-2 cursor-default text-neutral-700">
+        <div className="fixed bottom-5 right-5 flex items-center justify-center gap-2 text-neutral-700">
             <span className="text-2xl font-bold">Scroll</span>
             <FaMouse className="text-2xl" />
         </div>
